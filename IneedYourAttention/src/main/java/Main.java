bd74f3d238c8a07bbec94617cd127e769955f6ed
v0ncent <banksvincent5@gmail.com>
2023-03-22 18:54:45 -0500

    Created Main, started creating bot

---
+++ IneedYourAttention/src/main/java/Main.java
@@ -0,0 +1,10 @@
+import net.dv8tion.jda.api.JDABuilder;
+
+public class Main {
+
+    private final JDABuilder bot = JDABuilder.createDefault("");
+
+    public static void main(String[] args) {
+        // gateway intent stuff and build bot
+    }
+}
