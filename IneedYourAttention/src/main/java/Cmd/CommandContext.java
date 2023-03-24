36612b8736dc5508ceff9d1692cefe18137b3282
v0ncent <83373495+v0ncent@users.noreply.github.com>
2023-03-23 21:02:06 -0500

    set up bot to begin creating commands

--- IneedYourAttention/src/main/java/Cmd/CommandContext.java
+++ IneedYourAttention/src/main/java/Cmd/CommandContext.java
@@ -8,8 +8,6 @@ import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.sharding.ShardManager;
-
-import java.util.ArrayList;
 import java.util.List;
 
 public final class CommandContext {
